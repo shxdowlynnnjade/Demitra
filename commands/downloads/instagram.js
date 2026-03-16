865f55f994bdec48b23a4541d3786e52dba2e72e
shxdowlynnnjade <tvxuperlove@gmail.com>
2026-03-16 12:03:44 -0300

    Actualizar instagram.js

--- commands/downloads/instagram.js
+++ commands/downloads/instagram.js
@@ -22,20 +22,31 @@ let res = await fetch(api)
 let json = await res.json()
 
 if (!json.success) {
-return m.reply('No se pudo descargar el video.')
+return m.reply('No se pudo obtener el contenido.')
 }
 
-let video = json.result.downloadUrl[0]
+let media = json.result.downloadUrl[0]
+
+if (media.includes('.mp4')) {
 
 await client.sendMessage(m.chat,{
-video:{ url: video },
+video:{ url: media },
 mimetype:'video/mp4',
 caption:'Video descargado de Instagram'
 },{ quoted:m })
 
+} else {
+
+await client.sendMessage(m.chat,{
+image:{ url: media },
+caption:'Imagen descargada de Instagram'
+},{ quoted:m })
+
+}
+
 } catch (e) {
 console.log(e)
-m.reply('Error al descargar el video.')
+m.reply('Error al descargar el contenido.')
 }
 
 }
