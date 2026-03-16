d558a90b642c38dbc31ab67678ff51239145ee2e
shxdowlynnnjade <tvxuperlove@gmail.com>
2026-03-16 12:05:09 -0300

    Actualizar instagram.js

--- commands/downloads/instagram.js
+++ commands/downloads/instagram.js
@@ -16,33 +16,22 @@ return m.reply('El enlace no es válido.')
 
 try {
 
-let api = `https://api.nekolabs.my.id/downloader/instagram?url=${encodeURIComponent(args[0])}`
+let api = `https://api.delirius.xyz/download/instagram?url=${encodeURIComponent(args[0])}`
 
 let res = await fetch(api)
 let json = await res.json()
 
-if (!json.success) {
+if (!json.status) {
 return m.reply('No se pudo obtener el contenido.')
 }
 
-let media = json.result.downloadUrl[0]
-
-if (media.includes('.mp4')) {
+let media = json.data[0].url
 
 await client.sendMessage(m.chat,{
 video:{ url: media },
 mimetype:'video/mp4',
 caption:'Video descargado de Instagram'
 },{ quoted:m })
-
-} else {
-
-await client.sendMessage(m.chat,{
-image:{ url: media },
-caption:'Imagen descargada de Instagram'
-},{ quoted:m })
-
-}
 
 } catch (e) {
 console.log(e)
