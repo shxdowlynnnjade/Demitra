b8758fcc918f3ae1186b37cc551d8331dca53a08
shxdowlynnnjade <tvxuperlove@gmail.com>
2026-03-16 03:02:32 -0300

    Actualizar menu.js

--- commands/plugins/menu.js
+++ commands/plugins/menu.js
@@ -26,11 +26,87 @@ export default {
       }
 
       // Texto del menú
-      const menuTexto = `𖤐 ❖ 𝐙𝐄𝐑𝐎 𝐓𝐖𝐎'𝐒 𝐌𝐄𝐍𝐔 ❖ 𖤐
-❝ ¡Hola ${senderName}, ${saludo}~! ${carita}
-Soy *${botname}* y este es mi menú:
-${comandosCargados}
-𖤐 ~Zero Two 🌸 (´｡• ᵕ •｡\`)`;
+      const menuTexto = `░▒ㅤ🌸ㅤ＃DEMILOVEㅤㅤ＞ㅤ                                       ૮₍´ ˶ ｪ ˵ ₎ა                            〘ㅤ☆ㅤ〙
+
+                        𝐂𝐫𝐞𝐚𝐝𝐨𝐫 :: 
+> ㅤㅤ    @Demitra(adara)
+
+꒰ㅤ۪ㅤֺㅤ🪼੭ㅤ꧇ㅤBuenas tardes. Soy Demi, la sonrisa hecha código.
+
+꒰  👾 ꧇ㅤLe muestro mis comandos. Ordenados y afilados.
+
+
+        ࿙⃛͜࿚⃛࿙⃛͜࿚⃛࿙⃛͜࿚⃛࿙⃛͜࿚⃛   ୨୧  ࿙⃛͜࿚⃛࿙⃛͜࿚⃛࿙⃛͜࿚⃛࿙⃛͜࿚⃛
+
+
+> ㅤㅤ⧼ㅤ Principalㅤ⧽ㅤ🫐❝
+.reg
+.menu/help/menú/allmenu
+.infobot/infosocket
+.status/estado
+.report/reporte/sug/suggest
+
+
+
+> ㅤㅤ⧼ㅤ Set perfil?+ㅤ⧽ㅤ🪼❝
+.profile/perfil
+.setdescription/setdesc
+.setpasatiempo
+.sethobby
+.setgenre (hombre/mujer) 
+.marry/casarse @usuario
+.divorce
+.delpasatiempo
+.delgenre
+.removehobby
+.level/lvl
+.afk (estudiando, durmiendo, etc?) 
+
+> ㅤㅤ⧼ㅤ Grupo+ㅤ⧽ㅤ🍥❝
+.invite/invitar 
+.hidetag/tag
+.kick
+.todos/invocar/tagall
+.join link/unir link
+
+> ㅤㅤ⧼ㅤ Stickers+ㅤ⧽ㅤ🪼❝
+.bratt/brat texto
+.bratv
+.emojimix (Stickers emoji 😂+🪻)
+.qc texto (Stickers)
+.sticker/s
+
+> ㅤㅤ⧼ㅤ extras+ㅤ⧽ㅤ🍥❝
+.self
+.logout (subbapagado) 
+.reload (prendido) 
+.qrs/codes
+.pfp/getpic (fotos de perfil) 
+.lboard/lb/leaderboard (Ordena usuarios por experiencia total.)
+.hd/enhance/remini(Mejora calidad)
+.inspect/inspeccionar (Funciona para tener info del grupo)
+.readviewonce/read/rreadv/ver
+.say/decir (Reenvía.)
+.toimg /toimage (sticker a imagen)
+.tourl (Convierte en url) 
+
+> ㅤㅤ⧼ㅤ Mas+ㅤ⧽ㅤ🪷❝
+.translate/trad/traducir (Traduce) 
+.ia/chatgpt
+
+> ㅤㅤ⧼ㅤ Descargas+ㅤ⧽ㅤ🐢❝
+
+.ytsearch/search (búsqueda en youtube) 
+.play2/mp4/ytmp4/ytvideo/playvideo (videos descargados) 
+.play/mp3/ytmp3 /ytaudio/playaudio (Música audio)
+.tiktok / tt / tiktoksearch / ttsearch / tts (descarga y búsqueda) 
+.pinterest/pin (busqueda) 
+.instagram/ig
+.mediafire/mf
+.apk/aptoide/apkdl
+.imagen/img/image(Google imágenes)
+
+> © 2026 creado por Jade.`;
 
       // Enviar mensaje simple (texto)
       await client.sendMessage(m.chat, {
